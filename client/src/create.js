8d78ff7be05f0b9b4bf16e2b6cc4c2fd2ee8188f
Brendan Peercy <brendanpeercy@macbook-pro.lan>
2020-06-29 17:12:59 -0700

    added code for debugging

--- client/src/create.js
+++ client/src/create.js
@@ -2,5 +2,7 @@ import hostMainP from './host/HostMain'
 import guestMainP from './GuestMain'
 import './Global.css'
 
+if (process.env.ENV == 'debug') console.log('we are dubugging!')
+
 hostMainP()
 guestMainP()
