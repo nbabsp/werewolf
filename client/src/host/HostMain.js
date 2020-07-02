a31ed7e080b83014037059bdcbc17f752464929a
nbabsp <nbabsp@gmail.com>
2020-07-02 12:55:23 -0700

    Debug (#2)
    * added code for debugging
    
    * removed .env
    
    Co-authored-by: Brendan Peercy <brendanpeercy@macbook-pro.lan>

--- client/src/host/HostMain.js
+++ client/src/host/HostMain.js
@@ -10,6 +10,21 @@ let HostRequestor = {
     voteNowP: (gameId) => StaticRequestor.getP(`/games/${gameId}/voteNow`),
 }
 
+let registerPlayer = (name, gameName) => new Promise( async (resolve, reject) => {
+    let player = await StaticRequestor.postP('/players/register', {name: name})
+    let source = StaticRequestor.eventSource(`/games/find/${gameName}/${player.id}`)
+    source.onmessage = (e) => {
+        console.log('got message', e, e.data)
+        console.log('closing', player.id)
+        source.close()
+        resolve(e.data)
+    }
+    source.onerror = (e) => {
+        console.log('got an error', e)
+        source.close()
+    }
+})
+
 async function createGameP() {
     let lobby = document.createElement('host-controls')
     lobby.status = 'beforeGame'
@@ -24,7 +39,6 @@ async function createGameP() {
         }
     }
     document.body.appendChild(lobby)
-
 }
 
 async function hostGameP(gameId, gameName, deck, deckIds) {
@@ -33,6 +47,13 @@ async function hostGameP(gameId, gameName, deck, deckIds) {
     lobby.name = gameName
     lobby.deck = deck
     lobby.deckIds = deckIds
+    if (process.env.ENV == 'debug') {
+        await registerPlayer('a', gameName)
+        await registerPlayer('b', gameName)
+        lobby.deck = ['werewolf', 'werewolf', 'seer', 'robber', 'troublemaker', 'villager']
+        lobby.deckIds = ['werewolf1', 'werewolf2', 'seer', 'robber', 'troublemaker', 'villager1']
+    }
+
     lobby.names = ['John', 'Smith', 'Fred']
     lobby.startCallback = async () => {
         try {
