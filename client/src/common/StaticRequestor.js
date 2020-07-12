ccdc732023e0339d64333925c60157fac74ca489
nbabsp <nbabsp@gmail.com>
2020-07-11 17:11:07 -0700

    Pathway debug (#6)
    * allowed host to end and clean up the game
    
    * added debugging capabilities to switch between local and server
    
    Co-authored-by: Brendan Peercy <brendanpeercy@macbook-pro.lan>

--- client/src/common/StaticRequestor.js
+++ client/src/common/StaticRequestor.js
@@ -1,8 +1,14 @@
 import Requestor from './Requestor'
 
-let protocol = 'http'
-let host = 'localhost'
-let port = 9615
+let protocol = (process.env.HOST == 'local') ? 'http' : 'https'
+let host = (process.env.HOST == 'local') ? 'localhost' : 'werewolf.invades.space'
+let port = (process.env.HOST == 'local') ? 9615 : 443
+// let protocol = 'https'
+// let host = 'werewolf.invades.space'
+// let port = 443
+// let protocol = 'http'
+// let host = 'localhost'
+// let port = 9615
 
 let StaticRequestor = {
     getP: (path) => Requestor.getP(host, port, path),
